868d473e4765c0031c58dca75135be58b61a8a76
rop-lab <jhnycheru11@gmail.com>
2024-01-06 13:02:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// returnFirstTwoDrivers function
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+// returnLastTwoDrivers function
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+// selectingDrivers array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier function
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return fare * integer;
+    };
+};
+
+// fareDoubler function
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler function
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers function
+const selectDifferentDrivers = function(drivers, selectFunction) {
+    return selectFunction(drivers);
+};
